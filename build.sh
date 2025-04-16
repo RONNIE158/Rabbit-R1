40f420d7452db8bbfba05b60fc4d6c7504934e80
RONNIE158 <ronnie158@live.com>
2025-04-16 19:45:53 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,42 +1,45 @@
 #!/bin/bash
 
-# Prompt user for build type
-Clear
-echo "Select the build type:"
-echo "1. Original"
-echo "2. Android (No-Root)"
-echo "3. Switch Port"
-read -p "Enter your choice (1/2/3): " choice
+# Get build type from argument
+build_type=$1
 
-# Set build type and smali folder based on user input
-case $choice in
-  1) 
-    build_type="original"
+# Check if the argument was provided
+if [ -z "$build_type" ]; then
+  echo "Error: No build type provided."
+  exit 1
+fi
+
+echo "Selected build type: $build_type"
+
+# Set smali folder based on build type
+case $build_type in
+  original)
+    smali_folder="original/smali"
     ;;
-  2)
-    build_type="android (non root)"
+  "android (non root)")
+    smali_folder="android (non root)/smali"
     ;;
-  3)
-    build_type="switch port"
+  "switch port")
+    smali_folder="switch port/smali"
     ;;
   *)
-    echo "Invalid choice. Exiting."
+    echo "Invalid build type. Exiting."
     exit 1
     ;;
 esac
 
 # Build APK using apktool
-apktool b "${build_type}/smali" -o "${build_type}/apk/Rabbit R1 ${build_type}.apk"
+apktool b "$smali_folder" -o "$build_type/apk/Rabbit R1 $build_type.apk"
 
 # Sign the APK
-java -jar "./uber-apk-signer.jar" --apks "${build_type}/apk/Rabbit R1 ${build_type}.apk"
+java -jar "./uber-apk-signer.jar" --apks "$build_type/apk/Rabbit R1 $build_type.apk"
 
 # Rename the signed APK
-mv "${build_type}/apk/Rabbit R1 ${build_type}-aligned-debugSigned.apk" "${build_type}/apk/Rabbit R1 ${build_type}.apk"
+mv "$build_type/apk/Rabbit R1 $build_type-aligned-debugSigned.apk" "$build_type/apk/Rabbit R1 $build_type.apk"
 
 # Decompile the APK using jadx
-jadx -d "${build_type}/java" "${build_type}/apk/Rabbit R1 ${build_type}.apk"
+jadx -d "$build_type/java" "$build_type/apk/Rabbit R1 $build_type.apk"
 
-rm "${build_type}/apk/Rabbit R1 ${build_type}-aligned-debugSigned.apk.idsig"
+rm "$build_type/apk/Rabbit R1 $build_type-aligned-debugSigned.apk.idsig"
 
-echo "Build and decompile process completed for ${build_type} build."
+echo "Build and decompile process completed for $build_type build."
